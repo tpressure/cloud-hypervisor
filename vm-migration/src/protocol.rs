360998c446afcf39a64e9de7bb6f8d913de07e7d
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-09-02 13:22:23 +0200

    Revert "sort"
    This reverts commit a497ed3db569c92844ca747ab68e522c28e75b13.

--- vm-migration/src/protocol.rs
+++ vm-migration/src/protocol.rs
@@ -215,7 +215,7 @@ impl Response {
 }
 
 #[repr(C)]
-#[derive(Clone, Default, PartialEq, PartialOrd, Eq, Ord, Serialize, Deserialize)]
+#[derive(Clone, Default, PartialEq, Serialize, Deserialize)]
 pub struct MemoryRange {
     pub gpa: u64,
     pub length: u64,
@@ -308,7 +308,6 @@ impl MemoryRangeTable {
         for table in tables {
             data.extend(table.data);
         }
-        data.sort();
         data.dedup();
         Self { data }
     }
